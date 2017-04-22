e28fc909e129ba64084cadf69ba544176b1f9cb2
wokamoto <wokamoto1973@gmail.com>
2017-04-22 15:27:11 +0900

    fixed bug

--- route53.tf
+++ route53.tf
@@ -17,7 +17,7 @@ resource "aws_route53_record" "MX" {
 
 resource "aws_route53_record" "amazonses_verification_record" {
     zone_id = "${aws_route53_zone.public.id}"
-    name    = "${var.domain}"
+    name    = "_amazonses.${var.domain}"
     type    = "TXT"
     ttl     = "600"
     records = ["${aws_ses_domain_identity.domain.verification_token}"]
